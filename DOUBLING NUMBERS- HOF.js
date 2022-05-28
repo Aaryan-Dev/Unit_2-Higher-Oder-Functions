8b56fd3d6667103f162d162c70a62021e5d7c582
Aryan Dev <nobodygivesafak@gmail.com>
2022-05-29 01:54:11 +0530

    Create DOUBLING NUMBERS- HOF.js

---
+++ DOUBLING NUMBERS- HOF.js
@@ -0,0 +1,15 @@
+// Given an array of numbers print the square of those numbers
+
+// Given an array of numbers generate an array containing the double value of the numbers
+
+// Sample Input [1, 2] Sample Output [2, 4]
+
+
+
+let arr = [1, 2];
+
+let dob  = arr.map(function(el){
+  return el*2
+})
+
+console.log(dob);
